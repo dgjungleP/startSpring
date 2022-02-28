8cda616c5177886429e9c63f2b062f183147d8d2
Jungle <dgJungle_c@163.com>
2022-02-28 20:49:51 +0800

    find class

--- myspring/src/main/java/com/jungle/spring/Test.java
+++ myspring/src/main/java/com/jungle/spring/Test.java
@@ -6,7 +6,6 @@ import com.jungle.spring.mysrping.context.ApplicationContext;
 
 public class Test {
     public static void main(String[] args) {
-
         ApplicationContext applicationContext = new ApplicationContext(ApplicationConfig.class);
         Object service = applicationContext.getBean("userService");
     }
--- myspring/src/main/java/com/jungle/spring/mysrping/context/ApplicationContext.java
+++ myspring/src/main/java/com/jungle/spring/mysrping/context/ApplicationContext.java
@@ -1,5 +1,12 @@
 package com.jungle.spring.mysrping.context;
 
+import com.jungle.spring.mysrping.annotation.Component;
+import com.jungle.spring.mysrping.annotation.ComponentScan;
+
+import java.io.File;
+import java.lang.annotation.Annotation;
+import java.net.URL;
+
 /**
  * 对象容器
  */
@@ -13,10 +20,36 @@ public class ApplicationContext {
     }
 
     private void handleConfig() {
-        handleComponentScan();
+        try {
+            handleComponentScan();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void handleComponentScan() {
+    private void handleComponentScan() throws ClassNotFoundException {
+        if (!configClass.isAnnotationPresent(ComponentScan.class)) {
+            return;
+        }
+        ComponentScan componentScan = (ComponentScan) configClass.getDeclaredAnnotation(ComponentScan.class);
+        String path = componentScan.value();
+        System.out.println(path);
+        //扫描
+        //1、 搜寻路径下的所有类
+        ClassLoader loader = this.getClass().getClassLoader();
+        URL resource = loader.getResource(path.replace(".", "/"));
+        File file = new File(resource.getFile());
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            for (File f : files) {
+                String className = f.getPath();
+                className = className.substring(className.indexOf("com"), className.indexOf(".class")).replaceAll("\\\\", ".");
+                Class<?> clazz = loader.loadClass(className);
+                if (clazz.isAnnotationPresent(Component.class)) {
+
+                }
+            }
+        }
     }
 
     public Object getBean(String beanName) {
